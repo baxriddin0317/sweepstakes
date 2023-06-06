91266786624f380dfe879e0f01b8785e076b542d
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2023-06-06 22:49:20 +0500

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,285 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Link from "next/link";
+import React from "react";
+// import SolapeFooter from 'components/SolapeFooter';
+// import BottomBar from 'components/mobile/BottomBar';
+// import SolapeTopBar from '../components/SolapeTopBar';
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={`flex-1 bg-th-bkg-1 text-th-fgd-1 transition-all`}>
+      <header
+        style={{
+          padding: 0,
+          minHeight: 64,
+          height: "unset",
+          zIndex: "50",
+          position: "relative",
+        }}>
+        {/* <SolapeTopBar /> */}
+      </header>
+      <div className="max-w-[1536px] mx-auto flex-col py-8 px-4 xl:px-20">
+        {/* head */}
+        <div className="flex items-center flex-wrap justify-start">
+          <h1
+            className="text-xl text-white font-semibold sm:w-auto w-full
+            sm:mb-0 mb-3 sm:text-let text-center">
+            Swap Sweepstakes
+          </h1>
+          <div
+            className={
+              "flex md:ml-10 bg-orange-500 rounded-lg px-4 py-2 font-bold relative z-40 text-white text-xl"
+            }>
+            JUNE 2023
+          </div>
+          <div className="flex justify-end flex-grow">
+            <button
+              className={
+                "flex bg-th-bkg-3 rounded-lg px-4 py-2 font-bold relative z-40 text-md text-th-fgd-1 hover:text-orange-500 focus:outline-none disabled:cursor-not-allowed disabled:bg-th-bkg-4 disabled:text-th-fgd-4 md:hover:brightness-[1.1] md:disabled:hover:brightness-100"
+              }>
+              <a
+                href="https://solape.io/"
+                className="text-white"
+                rel="noopener noreferrer">
+                Return
+              </a>
+              <img
+                src="/solape_images/raffleimages/back-arrow.png"
+                alt="Crown"
+                className="ml-3 h-4"
+              />
+            </button>
+          </div>
+        </div>
+        {/* info cards */}
+        <div className="flex flex-wrap justify-center gap-6 w-full py-5">
+          <div className="mt-5 w-full lg:w-4/12 md:w-2/5 md:px-1 sm:px-10">
+            <div
+              className="rounded h-full
+                overflow-hidden bg-th-bkg-3">
+              <div
+                className="px-4 py-3 flex flex-wrap
+                    items-center bg-th-bkg-2">
+                <h3 className="text-white font-bold text-lg">General Info</h3>
+                <img
+                  src="/solape_images/raffleimages/info.png"
+                  alt="Info"
+                  className="ml-3 h-6"
+                />
+              </div>
+              <div className="py-3 px-4 text-sm" style={{ color: "#FFE5CC" }}>
+                <p className="mb-2">
+                  <span className="text-orange-500 font-bold">Swap </span>
+                  any amount of SPL tokens on solape.io and automatically enter
+                  the draw to win our monthly Sweepstakes!
+                </p>
+                <p className="mb-2">The more you swap, the greater the odds.</p>
+                <p>
+                  Winners are announced below & on our socials at the end of
+                  each month.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-5 w-full lg:w-4/12 md:w-2/5 md:px-1 sm:px-10">
+            <div
+              className="rounded h-full 
+                overflow-hidden"
+              style={{ backgroundColor: "#121616" }}>
+              <div className="py-3 px-4 text-sm" style={{ color: "#121616" }}>
+                <div className="relative">
+                  <div className="reward text-center relative">
+                    <img
+                      src="/solape_images/raffleimages/gift.png"
+                      alt="Reward"
+                      className="w-6 mx-auto"
+                    />
+                  </div>
+                  <div className="text-lg">
+                    <h2
+                      className="font-semibold
+                                "
+                      style={{ color: "#FFCB99" }}>
+                      PRIZES - RAFFLE #1
+                    </h2>
+                    <ul
+                      className="text-white font-extrabold
+                                italic
+                                pt-6 pl-8 numbered">
+                      <li className="mb-1 text-2xl">Okay Bear</li>
+                      <li className="mb-1 text-2xl">TFF</li>
+                      <li className="mb-1 text-2xl">The Heist</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="mt-5 w-full lg:w-2/12 md:w-1/2 md:px-1 px-10
+            sm:block hidden">
+            <div
+              className="rounded h-full
+                overflow-hidden"
+              style={{ backgroundColor: "#1C2222" }}>
+              <div
+                className="px-4 py-3 flex flex-wrap
+                    items-center"
+                style={{ backgroundColor: "#121616" }}>
+                <h3 className="text-white font-bold text-sm">Past Winners</h3>
+                <img
+                  src="/solape_images/raffleimages/crown.png"
+                  alt="Crown"
+                  className="ml-3 h-4"
+                />
+              </div>
+              <div className="py-3 px-4 text-sm">
+                <p className="mb-2 font-bold" style={{ color: "#FFE5CC" }}>
+                  2023
+                </p>
+                <p className="mb-2">
+                  <ul
+                    className="list-style-disc pl-4 text-white
+                            mb-3">
+                    <li>April</li>
+                    <li>May</li>
+                    <li>June</li>
+                    <li>July</li>
+                  </ul>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* img cards */}
+        <div className="flex flex-wrap gap-6 py-10 px-5 sm:px-10 lg:px-40">
+          <div className="max-w-md mx-auto rounded-xl md:max-w-2xl">
+            <div className="md:flex">
+              <div className="md:shrink-0">
+                <div className="img-border shadow-orange">
+                  <img
+                    src="/solape_images/raffleimages/image1.png"
+                    alt="Image 1"
+                    className="w-full"
+                  />
+                  <div
+                    className="px-4 pt-5 pb-3"
+                    style={{ backgroundColor: "#121616" }}>
+                    <h3 className="font-bold text-md text-white">Winner:</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-md mx-auto rounded-xl md:max-w-2xl">
+            <div className="md:flex">
+              <div className="md:shrink-0">
+                <div className="img-border">
+                  <img
+                    src="/solape_images/raffleimages/image2.png"
+                    alt="Image 2"
+                    className="w-full"
+                  />
+                  <div
+                    className="px-4 pt-5 pb-3"
+                    style={{ backgroundColor: "#121616" }}>
+                    <h3 className="font-bold text-md text-white">Winner:</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-md mx-auto rounded-xl md:max-w-2xl">
+            <div className="md:flex">
+              <div className="md:shrink-0">
+                <div className="img-border shadow-orange">
+                  <img
+                    src="solape_images/raffleimages/image3.png"
+                    alt="Image 3"
+                    className="w-full"
+                  />
+                  <div
+                    className="px-4 pt-5 pb-3"
+                    style={{ backgroundColor: "#121616" }}>
+                    <h3 className="font-bold text-md text-white">Winner:</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="mt-5 w-full md:w-4/12 sm:w-1/2 px-3 py-3
+            sm:hidden
+            block">
+            <div
+              className=" rounded h-full
+                overflow-hidden"
+              style={{ backgroundColor: "#1C2222" }}>
+              <div
+                className=" px-4 py-3 flex flex-wrap
+                    items-center"
+                style={{ backgroundColor: "#121616" }}>
+                <h3 className="text-white font-bold text-sm">Past Winners</h3>
+                <img
+                  src="/solape_images/raffleimages/crown.png"
+                  alt="Crown"
+                  className="ml-3 h-4"
+                />
+              </div>
+              <div className="py-3 px-4 text-sm" style={{ color: "#FFE5CC" }}>
+                <p className="mb-2 font-bold">2023</p>
+                <p className="mb-2">
+                  <ul
+                    className="list-style-disc pl-4 text-white
+                            mb-3">
+                    <li>April</li>
+                    <li>May</li>
+                    <li>June</li>
+                    <li>July</li>
+                  </ul>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* card link */}
+        <div className="text-center py-5">
+          <p className="text-lg" style={{ color: "#C5C5C5" }}>
+            For more information, please check our
+            <Link
+              href="https://www.discord.gg/solape/"
+              className=" link-underline
+                font-bold">
+              {" "}
+              Discord
+            </Link>
+            .
+          </p>
+        </div>
+        {/* end part */}
+        <div className="lg:px-60 md:px-40 px-10 py-10 sm:block hidden">
+          <div
+            className="particles px-4 py-2 flex
+            justify-between"
+            style={{ backgroundColor: "#1C2222" }}>
+            <h3 className="text-white font-bold text-lg mt-1">
+              Want to join the raffle?
+            </h3>
+            <button className="bg-green-500 rounded-md text-white px-3 text-sm font-bold py-1">
+              {" "}
+              <a
+                href="https://solape.io/"
+                className="text-white"
+                rel="noopener noreferrer">
+                Swap Here
+              </a>
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      {/* <BottomBar /> */}
+      {/* <SolapeFooter /> */}
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,115 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      display: ['Inter, sans-serif'],
+      body: ['Inter, sans-serif'],
+    },
     extend: {
+      animation: {
+        shake: 'shake 0.4s linear 4',
+        'spin-fast': 'spin 0.5s linear infinite',
+      },
+      cursor: {
+        help: 'help',
+      },
+      colors: {
+        'solape-theme': {
+          orange: {
+            DEFAULT: '#FF810A',
+            dark: '#F58700',
+          },
+          red: { DEFAULT: '#DE0269', dark: '#A2024C', muted: '#9B0149', bright: '#FD499D' },
+          green: { DEFAULT: '#0AD171', dark: '#08AF5F',  muted: '#067D43', bright: '#29F592' },
+          'bkg-1': '#000',
+          'bkg-2': '#121616',
+          'bkg-3': '#1C2222',
+          'bkg-4': '#38383D',
+          'fgd-1': '#D1D1D1',
+          'fgd-2': '#C8C8C8',
+          'fgd-3': '#B3B3B3',
+          'fgd-4': '#878787',
+          'fgd-5': '#FFCB99',
+          'bkg-button': '#E06C00',
+          'bkg-button-2': '#4E5152',
+          'bkg-warning': '#D1722B',
+          'bkg-raffle': '#F6C928',
+          'bkg-raffle-entered': '#0AD171',
+          'raffle-text-color': "#2F3737",
+          'raffle-ellipse-shadow': '#AE662A'
+
+        },
+        'bkg-warning': '#D1722B',
+        'bkg-raffle': '#F6C928',
+        'bkg-raffle-entered': '#0AD171',
+        'raffle-text-color': "#2F3737",
+        'raffle-ellipse-shadow': '#AE662A',
+        'th-bkg-1': 'var(--bkg-1)',
+        'th-bkg-2': 'var(--bkg-2)',
+        'th-bkg-3': 'var(--bkg-3)',
+        'th-bkg-4': 'var(--bkg-4)',
+        'th-fgd-1': 'var(--fgd-1)',
+        'th-fgd-2': 'var(--fgd-2)',
+        'th-fgd-3': 'var(--fgd-3)',
+        'th-fgd-4': 'var(--fgd-4)',
+        'th-fgd-5': 'var(--fgd-5)',
+        'th-primary': 'var(--primary)',
+        'th-primary-dark': 'var(--primary-dark)',
+        'th-red': 'var(--red)',
+        'th-red-dark': 'var(--red-dark)',
+        'th-red-muted': 'var(--red-muted)',
+        'th-red-bright': 'var(--red-bright)',
+        'th-green': 'var(--green)',
+        'th-green-dark': 'var(--green-dark)',
+        'th-green-muted': 'var(--green-muted)',
+        'th-green-bright': 'var(--green-bright)',
+        'th-orange': 'var(--orange)',
+        'th-bkg-button': 'var(--bkg-button)',
+        'th-bkg-button-2': 'var(--bkg-button-2)',
+
+      },
+      fontSize: {
+        xxs: '.65rem',
+      },
+      keyframes: {
+        shake: {
+          '0%, 100%': {
+            transform: 'rotate(0deg)',
+          },
+          '20%, 60%': {
+            transform: 'rotate(6deg)',
+          },
+          '40%, 80%': {
+            transform: 'rotate(-6deg)',
+          },
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'long-loss': "url('/share_images/bg-long-loss.png')",
+        'long-profit': "url('/share_images/bg-long-profit.png')",
+        'short-loss': "url('/share_images/bg-short-loss.png')",
+        'short-profit': "url('/share_images/bg-short-profit.png')",
+        'solape-swap-background': "url('/solape_images/solape-swap-bg.svg')",
+      },
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+        xxl: '1600px',
       },
     },
   },
+  // variants: {
+  //   extend: {
+  //     cursor: ['hover', 'focus', 'disabled'],
+  //     opacity: ['disabled'],
+  //     backgroundColor: ['disabled'],
+  //     textColor: ['disabled'],
+  //   },
+  // },
   plugins: [],
 }
